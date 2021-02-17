6e300b223ac6ce9ff26941d1783609c0c69780d3
Manny Morales <manny@movingartwork.net>
2021-02-17 09:33:34 -0500

    Added tree trunk and glow

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
 var doc=document,win=window,SZ,nums,nums2,temp,bidx,count;
 var abs=Math.abs,rnd=Math.random,round=Math.round,max=Math.max,min=Math.min;
-var ceil=Math.ceil,floor=Math.floor,sin=Math.sin,cos=Math.cos,PI=Math.PI;
+var sqrt=Math.sqrt,ceil=Math.ceil,floor=Math.floor;
+var sin=Math.sin,cos=Math.cos,PI=Math.PI;
 var CVS=doc.querySelector("#comp1"),CVS2=doc.querySelector("#comp2");
 var C=CVS.getContext("2d"),C2=CVS2.getContext("2d");
 function normInt(s){ return parseInt(s,32)-SZ }
 function d2r(n){ return n*PI/180 }
 function to1(n){ return n/255 };
 function to1N(n){ return n/128-1 };
-function clear( C ){ C.clearRect(0,0,SZ,SZ) }
-function getNums2(){
-	var hashSingles=[],seed,rvs,i=0;
-	seed = parseInt( tokenData.hash.slice(0,16), 16 );
-	for(i=0;i<64;i++) hashSingles.push(tokenData.hash.charAt(i));
-	rvs = hashSingles.map(n=>parseInt(n,16));
-	return rvs;
-}
 function getNums(){
-	var hashPairs=[],seed,rvs,i=0;
-	seed = parseInt( tokenData.hash.slice(0,16), 16 );
-	for(i=0;i<32;i++){
-		hashPairs.push(
-			tokenData.hash.slice( 2+(i*2),4+(i*2) )
-		);
+	var hashPairs=[],seed,rvs,i=0,j=0;
+	var hash = tokenData.hash.slice(2);
+	for(i=0;i<64;i++){
+		for(j=0;j<64;j++){
+			hashPairs.push( parseInt(
+				"0x" + hash.charAt(j) +
+				hash.charAt((j+i)%64)
+			));
+		}
 	}
-	rvs = hashPairs.map(n=>parseInt(n,16));
-	return rvs;
+	return hashPairs;
 }
 function xhrIsReady( xhr ){ return xhr.readyState === 4 && xhr.status === 200; }
 function xhrPOST( url, callback ){
@@ -44,6 +39,87 @@ function canvasAction( callback ){
 	callback( [].slice.call(arguments,1) );
 	C.restore()
 	C2.restore();
+}
+function boxBlurT(src,tgt,w,h,r,H){
+	var iarr=1/(r+r+1),i,j,ti,li,ri,fv,lv,val;
+	for(i=0; i<w; i++){
+		ti=i, li=ti, ri=ti+r*w, fv=src[ti], lv=src[ti+w*(h-1)], val=(r+1)*fv;
+		for(j=0; j<r; j++) val += src[ti+j*w];
+		for(j=0; j<=r; j++){
+			val += src[ri] - fv;
+			tgt[ti] = round( val*iarr ), ri+=w, ti+=w;
+		}
+		for( j=r+1; j<h-r; j++ ){
+			val += src[ri] - src[li];
+			tgt[ti] = round(val*iarr), li+=w, ri+=w, ti+=w;
+		}
+		for( j=h-r; j<h; j++ ){
+			val += lv - src[li];
+			tgt[ti] = round(val*iarr), li+=w, ti+=w;
+		}
+	}
+}
+function boxBlurH(src,tgt,w,h,r){
+	var iarr=1/(r+r+1),i,j,ti,li,ri,fv,lv,val;
+	for( i=0; i<h; i++) {
+		ti=i*w, li=ti, ri=ti+r, fv=src[ti], lv=src[ti+w-1], val=(r+1)*fv;
+		for( j=0; j<r; j++) val += src[ti+j];
+		for( j=0; j<=r; j++){
+			val += src[ri++] - fv;
+			tgt[ti++] = round(val*iarr);
+		}
+		for( j=r+1; j<w-r; j++){
+			val += src[ri++] - src[li++];
+			tgt[ti++] = round(val*iarr);
+		}
+		for( j=w-r; j<w; j++){
+			val += lv - src[li++];
+			tgt[ti++] = round(val*iarr);
+		}
+	}
+}
+function boxBlur(src,tgt,w,h,r){
+	for(var i=0; i<src.length; i++) tgt[i]=src[i];
+	boxBlurH(tgt,src,w,h,r);
+	boxBlurT(src,tgt,w,h,r);
+}
+function boxesForGauss(sigma, n){
+	var wIdeal,wl,wu,mIdeal,m,sizes=[],i;
+	wIdeal = sqrt( (12*sigma*sigma/n)+1 );
+	wl = floor(wIdeal);
+	if(wl%2==0) wl--;
+	wu = wl+2;
+	mIdeal = (12*sigma*sigma - n*wl*wl - 4*n*wl - 3*n)/(-4*wl - 4);
+	m = round(mIdeal);
+	for(i=0; i<n; i++) sizes.push(i<m?wl:wu);
+	return sizes;
+}
+function gaussBlur(src,tgt,w,h,r){
+	var bxs=boxesForGauss(r,3);
+	boxBlur(src,tgt,w,h,(bxs[0]-1)/2);
+	boxBlur(tgt,src,w,h,(bxs[1]-1)/2);
+	boxBlur(src,tgt,w,h,(bxs[2]-1)/2);
+	boxBlur(src,tgt,w,h,(bxs[3]-1)/2);
+}
+function fastBlur( amount, C ){
+	var w=h=SZ,c=[],i,j, d=C.getImageData(0,0,w,h);
+	for(i=0; i<4; i++){
+		c.push( new Uint8ClampedArray(d.data.length/4) );
+	}
+	for(i=0; i<d.data.length; i+=4){
+		for( j=0; j<c.length; j++ ){
+			c[j][i/4] = (d.data[i+j]);
+		}
+	}
+	for(i=0; i<c.length; i++){
+		gaussBlur(c[i],c[i],w,h,amount);
+	}
+	for(i=0; i<d.data.length; i+=4){
+		for( j=0; j<c.length; j++ ){
+			d.data[i+j] = c[j][i/4];
+		}
+	}
+	C.putImageData(d,0,0);
 }
 function addShape(shape, s, o, C){
 	var vs = shape.verts;
@@ -94,9 +170,19 @@ function transWithAnchor( x, y, C, callback ){
 	callback( [].slice.call(arguments,3) );
 	C.translate( x*-1, y*-1 );
 }
+function drawTrunk( args ){
+	var x = args[0], y = args[1], C = args[2];
+	C.lineWidth = bcount + bcount/2;
+	C.strokeStyle = "#000";
+	C.lineCap = "round";
+	C.beginPath();
+	C.moveTo( SZ/2, SZ*0.9 );
+	C.lineTo( x, y );
+	C.stroke();
+}
 function drawBranch( args ){
-	var n = args[0], x = args[1], y = args[2];
-	var sz=800/(bcount/2)*0.9, px, py, cx, cy, i, n;
+	var n = args[0], x = args[1], y = args[2], C = args[3];
+	var sz=500/(bcount/2)*0.9, px, py, cx, cy, i, n;
 	C.strokeStyle = "#000";
 	C.lineCap = "round";
 	for( i=0; i<n; i++ ){
@@ -109,21 +195,38 @@ function drawBranch( args ){
 		C.lineTo( cx, cy );
 		C.lineWidth = n-i;
 		C.stroke();
-		drawBranch( [n-i-1, cx, cy, nums[i]] );
+		drawBranch( [n-i-1, cx, cy, C] );
 		px = cx, py = cy;
 	}
+}
+function scratch( args ){
+	var a = args[0], b = args[1], n = args[2];
+	var px, py, cx, cy;
+	var vec = [ a[0]-b[0], a[1]-b[1] ], i, j;
+	var step = [ vec[0]/n, vec[1]/n ];
+	C.lineWidth = 1;
+	C.strokeStyle = "#000";
+	for(i=1;i<n.length;i++){
+		for(j=1;j<n.length;j++){
+			cx = a[0]+step[0]*j;
+			cy = a[1]+step[1]*j;
+			C.moveTo( px||a[0], py||a[1] );
+			C.lineTo( cx, cy );
+			px = cx, py = cy;
+		}
+	}
+	C.stroke();
 }
 function init(){
 	SZ = 800;
 	CVS.width = SZ;
 	CVS.height = SZ;
+	CVS2.width = SZ;
+	CVS2.height = SZ;
 	nums = getNums();
-	nums2 = getNums2();
 	bidx = 0;
 }
-function drawFloor(){
-	C.fillStyle = "#eee";
-}
+function drawFloor(){ C.fillStyle = "#eee" }
 function invert(){
 	C.globalCompositeOperation = "difference";
 	C.fillStyle = "#fff";
@@ -138,12 +241,28 @@ function drawBG(){
 	C.globalCompositeOperation = "multiply";
 	C.fillRect( 0, SZ*0.8, SZ, SZ );
 }
+function electrify(){
+	C.drawImage( CVS2, 0, 0 );
+	fastBlur( 20, C2 );
+	C2.globalCompositeOperation = "destination-out";
+	C2.fillStyle = "#ffffff80";
+	C2.fillRect( 0, 0, SZ, SZ );
+	C.drawImage( CVS2, 0, 0 );
+	canvasAction( invert );
+}
 function handleKeyPress(){}
 function render(){
 	bcount = round( to1(nums[0])*6 ) + 6;
 	canvasAction( drawBG );
-	canvasAction( drawBranch, bcount, SZ/2, SZ*0.9 );
-	if ( nums[9] > 128 ) canvasAction( invert );
+	var x = to1N(nums[5])*(SZ/10) + SZ/2, y = SZ*0.6;
+	canvasAction( drawTrunk, x, y, C2 );
+	canvasAction( drawBranch, bcount, x, y, C2 );
+	if ( nums[9] > 127 ){
+		electrify();
+	} else {
+		C.drawImage( CVS2, 0, 0 );
+	}
+	C2.clearRect( 0, 0, SZ, SZ );
 }
 function main(){
 	init();
